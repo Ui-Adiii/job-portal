258c0650928e39053915a10011c345506c939b6f
ui-adiii <beaheroinyourlife@gmail.com>
2025-07-17 23:19:45 +0530

    adding store part

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import { Button } from './components/ui/button'
+import { useDispatch, useSelector } from 'react-redux'
+import { toggleTheme } from './store/theme/themeSlice'
+
 const App = () => {
+  const {theme} =useSelector((state)=>state.theme)
+  const dispatch=useDispatch();
+
+  console.log(theme);
   return (
     <div >
+      <Button
+      onClick={()=>dispatch(toggleTheme())}
+      >Click</Button>
     </div>
   )
 }
